39de71ba21a20ab1b2e7bb0dd183142b5a1c9642
Léonard <epoc4oftime@gmail.com>
2020-03-06 11:09:06 +0100

    double yes

---
+++ Neural Network/Assets/CheckPointManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPointManager : MonoBehaviour
+{
+    public static CheckPointManager instance;
+    public Transform firstCheckPoint;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    [ContextMenu("Init CheckPoint")]
+    public void Init()
+    {
+        firstCheckPoint = transform.GetChild(0);
+
+        for (int i = 0; i < transform.childCount-1; i++)
+        {
+            transform.GetChild(i).GetComponent<CheckPoint>().nextCheckpoint = transform.GetChild(i + 1);
+        }
+
+        transform.GetChild(transform.childCount - 1).GetComponent<CheckPoint>().nextCheckpoint = transform.GetChild(0);
+    }
+}
